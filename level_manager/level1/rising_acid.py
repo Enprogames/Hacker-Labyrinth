fe876198491bd70640797056aed536722a211ee9
ethan <e_posner@outlook.com>
2022-11-06 16:28:18 -0800

    Created initial rising acid game

--- level_manager/level1/rising_acid.py
+++ level_manager/level1/rising_acid.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 
+import os
 import random
+from pathlib import Path
 
 
 class BlockSet:
@@ -14,23 +16,29 @@ class BlockSet:
         self.height = random.randint(min, max)
 
         for i in range(width):
-            first_block = self.height - random.randint(min+1, max-1)
+            first_block = self.height - random.randint(1, self.height-1)
             second_block = self.height-first_block
             self.blocks.append(first_block)
             self.blocks.append(second_block)
 
+        random.shuffle(self.blocks)
 
-def test_answer(answer: int, blocks: list) -> bool:
-    """Return whether or not the given height can be used to stack the blocks into pairs of two
 
-    Args:
-        answer (int): _description_
-        blocks (list): _description_
+def read_prompt_file(prompt_file: str) -> str:
+    initial_prompt = ""
+    prompt_file_dir: os.PathLike = None
+    if Path('level_manager', 'level1', prompt_file).exists():
+        prompt_file_dir = Path('level_manager', 'level1')
+    elif Path('level1', prompt_file).exists():
+        prompt_file_dir = Path('level1')
+    elif Path('', prompt_file).exists():
+        prompt_file_dir = Path('', prompt_file)
+    else:
+        raise OSError(f"The file {prompt_file} couldn't be found.")
+    with open(Path(prompt_file_dir, prompt_file), encoding="utf8") as fp:
+        initial_prompt = fp.read()
 
-    Returns:
-        bool: _description_
-    """
-    pass
+    return initial_prompt
 
 
 if __name__ == '__main__':
@@ -38,12 +46,16 @@ if __name__ == '__main__':
     retries = 3
     min_height, max_height, width = random.randint(5, 10), random.randint(10, 20), random.randint(5, 20)
     
-    # give initial prompt
     blocks_set = BlockSet()
     blocks_set.generate_blocks(width, min_height, max_height)
     guess = None
-    print("Welcome to the puzzle.")
-    print(f"The heights are {blocks_set.blocks}")
+
+    # give initial prompt
+    initial_prompt = read_prompt_file('initial_prompt.txt')
+    
+    print(initial_prompt)
+    print(f"You arrange the blocks and find that they have the following heights:\n{blocks_set.blocks}")
+    print("How high must you stack the blocks?")
 
     # if 
     while retries >= 1 and guess != blocks_set.height:
