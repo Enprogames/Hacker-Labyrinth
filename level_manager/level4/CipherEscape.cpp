6dc03b7f18c5041ae8b4249496154a0ae919cdbe
Cassius Kerman <cassius9@hotmail.ca>
2022-11-16 01:47:21 -0800

    level4 files fixed

--- level_manager/level4/CipherEscape.cpp
+++ level_manager/level4/CipherEscape.cpp
@@ -1,106 +1,118 @@
 // CipherEscape.cpp
-#include <iostream>;
-#include "CipherEscape.h";
+#include <iostream>
+#include "CipherEscape.h"
 using std::cin;
 using std::cerr;
 using std::cout;
 using std::endl;
 using std::string;
 
-// The player is given the following prompt:
-int CipherEscape()
-{
-  int count = 0;
-  const int correctPasscode = 6924;
-  string userInput;
+// Usable commands
+const string touch = "touch";
+const string listen = "listen";
+const string useSwitch = "useSwitch";
+const string look = "look";
+const string usePINpad = "usePINpad";
 
+const int correctPasscode = 6924;
+
+int main()
+{
+  // Opening message to player
   cout << "You find yourself in a small, dark room. You cannot see anything." << endl;
   cout << "You hear a voice on the loudspeaker:" << endl;
-  cout << "'You must use critical thinking skills and decoders to solve the " <<;
-  cout << "riddles of this room. You will have 10 minutes to escape before " <<;
-  cout << "the room fills with a poisonous gas. Enter in actions, such as " <<;
-  cout << "'touch', 'look', or 'use object', to interact with the room, and" <<;
-  cout << "decode messages to progress. Good luck player.'" << endl;
+  cout << "'You must use critical thinking skills and decoders to solve the ";
+  cout << "riddles of this room. You will have 10 minutes to escape " << endl;
+  cout << "before the room fills with a poisonous gas. Enter in single word actions, ";
+  cout << "such as 'touch', 'look', or 'useObject', to interact with the room, ";
+  cout << "and decode messages to progress. If nothing happens when ";
+  cout << "you input a command, try something different. " << endl;
+  cout << "Good luck player. Now, begin.'" << endl;
+  cout << " " << endl;
 
-  // The player will then have to enter a command, the corrent command here is to
-  // "touch/feel the wall"
+  // The player will then have to enter a command
 
-  getline (cin, userInput);
-  AnswerCheck(int count, string userInput);
+  int count = 0;
+  string userInput;
 
-  // > touch/feel -> if correct count++
+  cin >> userInput;
+  AnswerCheck(count, userInput);
 
-  count++;
-
+  // > touch -> if correct count++
   // count keeps track of what stage of the level the player is on
-  // count = 1
 
-  cout << "You reach out and feel the wall in front of you" << endl;
-  cout << "You can make out distinct bumps:" << endl;
+  cout << " " << endl;
+  cout << "You reach out and touch the cold, stone wall in front of you." << endl;
+  cout << "You can make out several distinct bumps:" << endl;
   cout << " " << endl;
   cout << ".    .    .    .  .    . .     . .  .  .      .  .    .  . ." << endl;
   cout << ".  .    .    . .    .    .          .    .  .      .  .    ." << endl;
   cout << ".       .    .         .            .  .    .         .  . ." << endl;
+  cout << " " << endl;
+
+  count++; // count = 1
 
   // Answer: listen closely
-  // Player must now enter a command once they've deciphered the message,
-  // the correct command here is to "listen/listen closely"
+  // Player must enter a command once they've deciphered the message
 
-  getline (cin, userInput);
-  AnswerCheck(int count, string userInput);
+  cin >> userInput;
+  AnswerCheck(count, userInput);
 
-  // > listen/listen closely/hear
+  // > listen
+
+  cout << " " << endl;
+  cout << "You focus your hearing. Suddenly, a faint tone can be heard beeping:" << endl;
+  cout << " " << endl;
+  cout << ".-.. .. --. .... - / ... .-- .. - -.-. .... / .- -... --- ...- ." << endl;
+  cout << " " << endl;
+
+  // Answer: light switch above
 
   count++; // count = 2
 
-  cout << "You focus your hearing. Suddenly, a faint tone can be heard beeping:" << endl;
+  cin >> userInput;
+  AnswerCheck(count, userInput);
 
-  cout << ".-.. .. --. .... - / ... .-- .. - -.-. .... / .- -... --- ...- ." << endl;
+  // > useSwitch
 
-  // Answer: light switch above
-  // Player must now enter a command once they've deciphered the message,
-  // the correct command here is to "touch the switch/flip the switch"
-
-  getline (cin, userInput);
-  AnswerCheck(int count, string userInput);
-
-  // > touch switch/flip switch/reach switch/use switch
+  cout << " " << endl;
+  cout << "You reach up high for the switch and manage to flip it on." << endl;
+  cout << "A dimly lit lightbulb emits an orange glow in the room." << endl;
+  cout << " " << endl;
 
   count++; // count = 3
 
-  cout << "You reach up high for the switch and manage to flip it on." << endl;
-  cout << "A dimly lit lightbulb emits an orange glow in the room." << endl;
+  cin >> userInput;
+  AnswerCheck(count, userInput);
 
-  getline (cin, userInput);
-  AnswerCheck(int count, string userInput);
+  // > look
 
-  // > look/look around/see
+  cout << " " << endl;
+  cout << "You look around and see a metal door with a PINpad on the wall to your right," << endl;
+  cout << "and three sets of codes carved into the wall to your left:" << endl;
+  cout << " " << endl;
+  cout << "01100010 01100001 01110011 01100101 00110110 00110100" << endl;
+  cout << " " << endl;
+  cout << "QVNDSUk=" << endl;
+  cout << " " << endl;
+  cout << "83 73 88 57 84 87 79 52" << endl;
+  cout << " " << endl;
+
+  /* Answer of Binary: Base64
+     Answer of Base64: ASCII
+     Answer of ASCII: SIX 9 TWO 4 */
 
   count++; // count = 4
 
-  cout << "You look around and see three sets of codes written on the wall to your " << endl;
-  cout << "left, and a door with a PIN pad on the wall to your right. The codes are:" << endl;
+  cin >> userInput;
+  AnswerCheck(count, userInput);
 
-  cout << "01100010 01100001 01110011 01100101 00110110 00110100" << endl;
+  // > usePINpad
 
-  cout << "QVNDSUk=" << endl;
-
-  cout << "83 73 88 57 84 87 79 52" << endl;
-
-  /* Answer of Binary: Base64
-    Answer of Base64: ASCII
-    Answer of ASCII: SIX 9 TWO 4 */
-
-  // Player must now enter a command once they've deciphered the messages,
-  // the correct command here is to "touch the PIN pad/use the PIN pad"
-
-  getline (cin, userInput);
-  AnswerCheck(int count, string userInput);
-
-  // > Use PIN pad/touch PIN pad/touch/feel
-
-  cout << "You touch the PIN pad, the display turns on and message reads:" << endl;
+  cout << " " << endl;
+  cout << "You use the PINpad. The display turns on and a message reads:" << endl;
   cout << "'Enter a 4-digit number:'" << endl;
+  cout << " " << endl;
 
   // Player must now enter the passcode they deciphered from the previous set of
   // messages, the correct passcode is designated as a 4-digit integer
@@ -108,64 +120,76 @@ int CipherEscape()
   int passcode;
   cin >> passcode;
 
-  while (passcode != correctPasscode) {
+  while(passcode != correctPasscode) {
     cin.clear();
     cin.ignore(100,'\n');
+    cout << " " << endl;
     cout << "Incorrect passcode." << endl;
     cout << "Please try again." << endl;
+    cout << " " << endl;
     cin >> passcode;
   }
   // > 6924
 
   // With the correct 4-digit passcode inputted, the player completes the level.
   // Any incorrect commands will display the message: “[user input] is not recognized as a command”.
-  // For the PIN pad, the error message “Incorrect passcode” will be displayed
-  cout << "You enter '6924' on the PIN pad. With a beep, the door unlocks " << endl;
-  cout << "and slowly opens up. Congratulations, you've completed the level!" << endl;
+  // For the PIN pad, the error message “Incorrect passcode.” will be displayed
+
+  cout << " " << endl;
+  cout << "You enter " << correctPasscode << " on the PIN pad. With a beep, the door unlocks " << endl;
+  cout << "and slowly swings open. Freedom. Congratulations, you've completed the level!" << endl;
+  cout << " " << endl;
+
   return 0;
 }
 
 void AnswerCheck(int count, string userInput)
 {
-  if (count == 0) {
-    while (userInput != "touch" || userInput != "feel") { // loop while input is invalid
-      cout << userInput << "is not recognized as a command." << endl;
-      cout << "Please try again." << endl;
+  if(count == 0) {
+    while(userInput != touch) { // loop while input is invalid
+      cout << " " << endl;
+      cout << "Nothing happens." << endl;
+      cout << " " << endl;
       cin.clear();
       cin.ignore(100,'\n');
-      getline (cin, userInput);
+      cin >> userInput;
     }
-  } else if (count == 1) {
-    while (userInput != "listen" || userInput != "hear") { // loop while input is invalid
-      cout << userInput << "is not recognized as a command." << endl;
-      cout << "Please try again." << endl;
+  } else if(count == 1) {
+    while(userInput != listen) { // loop while input is invalid
+      cout << " " << endl;
+      cout << "Nothing happens." << endl;
+      cout << " " << endl;
       cin.clear();
       cin.ignore(100,'\n');
-      getline (cin, userInput);
+      cin >> userInput;
     }
-  } else if (count == 2) {
-    while (userInput != "use switch" || userInput != "flip switch") { // loop while input is invalid
-      cout << userInput << "is not recognized as a command." << endl;
-      cout << "Please try again." << endl;
+  } else if(count == 2) {
+    while(userInput != useSwitch) { // loop while input is invalid
+      cout << " " << endl;
+      cout << "Nothing happens." << endl;
+      cout << " " << endl;
       cin.clear();
       cin.ignore(100,'\n');
-      getline (cin, userInput);
+      cin >> userInput;
     }
-  } else if (count == 3) {
-    while (userInput != "look" || userInput != "see") { // loop while input is invalid
-      cout << userInput << "is not recognized as a command." << endl;
-      cout << "Please try again." << endl;
+  } else if(count == 3) {
+    while(userInput != look) { // loop while input is invalid
+      cout << " " << endl;
+      cout << "Nothing happens." << endl;
+      cout << " " << endl;
       cin.clear();
       cin.ignore(100,'\n');
-      getline (cin, userInput);
+      cin >> userInput;
     }
   } else if (count == 4) {
-    while (userInput != "use PIN pad" || userInput != "touch PIN pad") { // loop while input is invalid
-      cout << userInput << "is not recognized as a command." << endl;
-      cout << "Please try again." << endl;
+    while (userInput != usePINpad) { // loop while input is invalid
+    cout << " " << endl;
+    cout << "Something happens, it was nothing." << endl;
+    cout << " " << endl;
       cin.clear();
       cin.ignore(100,'\n');
-      getline (cin, userInput);
+      cin >> userInput;
     }
   }
+  return;
 }
--- level_manager/level4/CipherEscape.h
+++ level_manager/level4/CipherEscape.h
@@ -2,8 +2,5 @@
 #pragma once
 #include <string>
 
-// main routine for CipherEscape level
-int CipherEscape();
-
 // answer checking function for user's string inputs
 void AnswerCheck(int count, std::string userInput);
