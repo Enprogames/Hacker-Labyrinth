08c54abc8ed621c1c24b256715a21657e1fbbb05
ethan <e_posner@outlook.com>
2022-11-20 14:55:24 -0800

    Improved instructions for rising acid game

--- level_manager/level1/rising_acid.py
+++ level_manager/level1/rising_acid.py
@@ -12,16 +12,46 @@ class BlockSet:
         self.answer = -1
         self.height = -1
 
-    def generate_blocks(self, width: int, min: int, max: int):
-        self.height = random.randint(min, max)
+    def generate_blocks(self, width: int, min_height: int, max_height: int):
+        self.height = random.randint(min_height+1, max_height)
 
         for i in range(width):
-            first_block = self.height - random.randint(1, self.height-1)
+            first_block = self.height - random.randint(min_height, self.height-1)
             second_block = self.height-first_block
             self.blocks.append(first_block)
             self.blocks.append(second_block)
 
         random.shuffle(self.blocks)
+
+    def print_blocks(self):
+        output = ""
+
+        max_height = max(self.blocks)
+        for current_level in range(max_height+1):
+            
+            for block_height in self.blocks:
+                if current_level < max_height:
+                    if max_height-current_level <= block_height:
+                        output += '##'
+                    else:
+                        output += '  '
+                else:
+                    output += f"{block_height:<2}"
+                output += '  '
+            output += '\n'
+
+        print(output)
+
+    def print_completed_wall(self):
+        output = ''
+        for current_level in range(self.height):
+            for i in range(len(self.blocks)):
+                output += '##'
+                if i < len(self.blocks)-1:
+                    output += ' '
+            output += '\n'
+
+        print(output)
 
 
 def read_prompt_file(prompt_file: str) -> str:
@@ -41,7 +71,7 @@ if __name__ == '__main__':
     retries = 3
     guess = None
     
-    min_height, max_height, width = random.randint(5, 10), random.randint(10, 20), random.randint(5, 20)
+    min_height, max_height, width = random.randint(5, 10), random.randint(10, 20), random.randint(3, 10)
     blocks_set = BlockSet()
     blocks_set.generate_blocks(width, min_height, max_height)
 
@@ -49,23 +79,31 @@ if __name__ == '__main__':
     initial_prompt = read_prompt_file('initial_prompt.txt')
     
     print(initial_prompt)
-    print(f"You arrange the blocks and find that they have the following heights:\n{blocks_set.blocks}")
+    print("You arrange the blocks and find that they have the following heights:\n")
+    blocks_set.print_blocks()
     print("How high must you stack the blocks?")
 
     # continue looping until an answer is satisfactory or the user runs out of retries
     while retries >= 1 and guess != blocks_set.height:
-        guess = input(f"Enter your guess. You have {retries} tries left:\n")
-        try:
-            old_guess = guess
-            guess = int(guess)
-        except ValueError:
-            guess = old_guess
-
-        if guess == blocks_set.height:
-            print(f"Congratulations! Your guess of {guess} was correct!")
+        guess = input(f"Enter your guess. You have {retries} tries left. Enter (H) for help:\n")
+        if guess.lower() == 'h':
+            print("""The goal of the game is to stack the blocks in pairs of 2 so that they create a wall of uniform height.
+                     The wall is used to block the acid. For example, if you had one block of height 2, and another of
+                     height 3, you could combine them to make a stack of height 5.""")
         else:
-            print(f"{guess} is not correct.")
-            retries -= 1
+            try:
+                old_guess = guess
+                guess = int(guess)
+            except ValueError:
+                guess = old_guess
+
+            if guess == blocks_set.height:
+                print(f"Congratulations! Your guess of {guess} was correct!")
+                blocks_set.print_completed_wall()
+                print("You build a wall and it successfully blocks the acid.")
+            else:
+                print(f"{guess} is not correct.")
+                retries -= 1
 
     if retries == 0 and guess != blocks_set.height:
         print(f"The correct answer was {blocks_set.height}")
